c2d8d69c7352763b86d2e747047592a566c3d89e
Danilo C. Graciano <danilocgraciano@gmail.com>
2019-06-04 21:01:22 -0300

    Inclusão de espaço entre o valor e o símbolo monetário.

--- Financas/app/src/main/java/com/example/financas/extension/BigDecimalExtension.kt
+++ Financas/app/src/main/java/com/example/financas/extension/BigDecimalExtension.kt
@@ -2,8 +2,11 @@ package com.example.financas.extension
 
 import java.math.BigDecimal
 import java.text.DecimalFormat
+import java.text.NumberFormat
 import java.util.Locale
 
 fun BigDecimal.format(locale: Locale = Locale("pt", "br")): String {
-    return DecimalFormat.getCurrencyInstance(locale).format(this)
+    val currencyInstance: NumberFormat = DecimalFormat.getCurrencyInstance(locale)
+    val symbol: String = currencyInstance.currency.symbol
+    return currencyInstance.format(this).replace(symbol, "${symbol} ")
 }
